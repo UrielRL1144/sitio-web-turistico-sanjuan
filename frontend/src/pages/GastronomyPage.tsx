66ef8395f4f73d9f0fb08e59c4731c2b466349ff
Uriel Ramos <luciouriel646@gmail.com>
2025-10-13 13:49:53 -0600

    Agregar página GastronomyPage con secciones de gastronomía y platillos.

---
+++ frontend/src/pages/GastronomyPage.tsx
@@ -0,0 +1,12 @@
+// GastronomyPage.tsx
+import { GastronomySection } from "@/cultura/section-gastronomia/GastronomySection";
+import { PlatillosSection } from "@/cultura/section-gastronomia/PlatillosSection";
+
+export function GastronomyPage() {
+  return (
+    <>
+      <GastronomySection />
+      <PlatillosSection />
+    </>
+  );
+}
