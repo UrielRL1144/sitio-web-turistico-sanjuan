4989284990fb32e980fc85ac4e2edbe95dba7007
Uriel Ramos <luciouriel646@gmail.com>
2025-10-13 13:49:43 -0600

    Agregar componente CultureHome y reorganizar secciones en CulturePage

--- frontend/src/pages/CulturePage.tsx
+++ frontend/src/pages/CulturePage.tsx
@@ -1,12 +1,16 @@
+import { CultureHome } from '@/cultura/CultureHome';
 import { CultureSection } from '@/cultura/CultureSection';
 import { CultureLanguageSection } from '@/cultura/CultureLanguageSection';
 import { CultureGastronomySection } from '@/cultura/CultureGastronomy';
 import { CultureDance } from '@/cultura/CultureDance';
+import { CultureArtesanias } from '@/cultura/CultureArtesanias';
 export function CulturePage() {
   return (
     <>
+      <CultureHome />
       <CultureSection />
       <CultureLanguageSection/>
+      <CultureArtesanias/>
       <CultureGastronomySection/>
       <CultureDance/>
     </>
