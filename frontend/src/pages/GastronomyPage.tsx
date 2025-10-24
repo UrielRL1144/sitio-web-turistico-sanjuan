0102133ce807003127177f6b35139689d372ff3a
Uriel Ramos <luciouriel646@gmail.com>
2025-10-24 14:34:57 -0600

    Se agrego a ViajeSensorialGastronomico para ser la segunda secci{on de gastronomia}

--- frontend/src/pages/GastronomyPage.tsx
+++ frontend/src/pages/GastronomyPage.tsx
@@ -1,11 +1,13 @@
 // GastronomyPage.tsx
 import { GastronomySection } from "@/cultura/section-gastronomia/GastronomySection";
 import { PlatillosSection } from "@/cultura/section-gastronomia/PlatillosSection";
+import { ViajeSensorialGastronomico } from "@/cultura/section-gastronomia/ViajeSensorialGastronomico";
 
 export function GastronomyPage() {
   return (
     <>
       <GastronomySection />
+      <ViajeSensorialGastronomico />
       <PlatillosSection />
     </>
   );
