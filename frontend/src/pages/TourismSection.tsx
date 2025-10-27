0a5a2e91d8d571ada99e69e036f26e1719edd841
Uriel Ramos <luciouriel646@gmail.com>
2025-10-27 13:03:58 -0600

    Se utilizo el scroll simple para manejar las secciones de turismo y places

--- frontend/src/pages/TourismSection.tsx
+++ frontend/src/pages/TourismSection.tsx
@@ -1,16 +1,30 @@
+import { HeroSection } from '@/turismo/HeroSection';
+import { useRef } from 'react';
 import  Places  from '../turismo/Places';
 import { AdventureActivities } from '../turismo/AdventureActivities ';
 
-
-
 export function TourismSection() {
+  // ELIMINAR las refs y usar IDs directamente
+  //manera facil de hacer scrooll de la manera mas simple
+  
+  const handleScrollToHighlights = () => {
+    document.getElementById('places')?.scrollIntoView({ behavior: 'smooth' });
+  };
+    const cardsRef = useRef<HTMLDivElement>(null);
+
+    
+    const handleScrollToCards = () => {
+      cardsRef.current?.scrollIntoView({ behavior: 'smooth' });
+    };
   return (
     <>
+      <HeroSection
+      onDiscoverClick={handleScrollToHighlights} 
+        onDiscoverCardClick={handleScrollToCards} />
       {/* Sección principal de lugares destacados */}
-      <Places />
-
+      <Places/>
       {/* Actividades de aventura */}
-      <AdventureActivities />
+      <AdventureActivities ref={cardsRef}/>
 
       
     </>
